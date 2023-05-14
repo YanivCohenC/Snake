05907f22ec63751fe0dcdf4b1b99f8a1d3958999
Yaniv Cohen <yaniv.cohen.c@gmail.com>
2023-05-14 19:13:50 +0300

    Created Buttons and Quit function

--- Snake/Menu.Designer.cs
+++ Snake/Menu.Designer.cs
@@ -29,23 +29,74 @@ partial class Menu
     private void InitializeComponent()
     {
         pictureBox1 = new PictureBox();
+        button1 = new Button();
+        button2 = new Button();
+        button3 = new Button();
+        button4 = new Button();
         ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
         SuspendLayout();
         // 
         // pictureBox1
         // 
-        pictureBox1.Location = new Point(277, 35);
+        pictureBox1.Location = new Point(514, 75);
+        pictureBox1.Margin = new Padding(6);
         pictureBox1.Name = "pictureBox1";
-        pictureBox1.Size = new Size(251, 128);
+        pictureBox1.Size = new Size(466, 273);
         pictureBox1.TabIndex = 0;
         pictureBox1.TabStop = false;
         // 
+        // button1
+        // 
+        button1.Location = new Point(594, 395);
+        button1.Name = "button1";
+        button1.Size = new Size(319, 96);
+        button1.TabIndex = 1;
+        button1.Text = "New Game";
+        button1.UseVisualStyleBackColor = true;
+        button1.Click += newGame_Click;
+        // 
+        // button2
+        // 
+        button2.Enabled = false;
+        button2.Location = new Point(594, 497);
+        button2.Name = "button2";
+        button2.Size = new Size(319, 96);
+        button2.TabIndex = 2;
+        button2.Text = "Continue";
+        button2.UseVisualStyleBackColor = true;
+        button2.Click += continueGame_Click;
+        // 
+        // button3
+        // 
+        button3.Location = new Point(594, 599);
+        button3.Name = "button3";
+        button3.Size = new Size(319, 96);
+        button3.TabIndex = 3;
+        button3.Text = "Scoreboard";
+        button3.UseVisualStyleBackColor = true;
+        button3.Click += openScoreBoard_Click;
+        // 
+        // button4
+        // 
+        button4.Location = new Point(594, 701);
+        button4.Name = "button4";
+        button4.Size = new Size(319, 96);
+        button4.TabIndex = 4;
+        button4.Text = "Quit";
+        button4.UseVisualStyleBackColor = true;
+        button4.Click += quitGame_Click;
+        // 
         // Menu
         // 
-        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleDimensions = new SizeF(13F, 32F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
+        ClientSize = new Size(1486, 960);
+        Controls.Add(button4);
+        Controls.Add(button3);
+        Controls.Add(button2);
+        Controls.Add(button1);
         Controls.Add(pictureBox1);
+        Margin = new Padding(6);
         Name = "Menu";
         Text = "Form1";
         ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -55,4 +106,8 @@ partial class Menu
     #endregion
 
     private PictureBox pictureBox1;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 }
--- Snake/Menu.cs
+++ Snake/Menu.cs
@@ -6,4 +6,24 @@ public partial class Menu : Form
     {
         InitializeComponent();
     }
+
+    private void newGame_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void continueGame_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void openScoreBoard_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void quitGame_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
 }
